8aac54ae4a6f7dfdd2ff08f56e5528db95333dac
Capeeslash <kenyoti.p@gmail.com>
2023-05-23 20:02:05 +0800

    Addressed requirements of the Portfolio

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-""""Demonstrates the use of the Smiley class and its subclasses.
-If you have access to a SenseHAT (either via a Raspberry Pi or a SenseHAT emulator), you can use the real SenseHAT class instead of the mock SenseHAT class.
-That is, delete the sense_hat.py file that is included in this bundle."""
+
 import time
 
-from happy import Happy
+from sad import Sad
 
 if __name__ == '__main__':
     # This is only needed if you have not deleted sense_hat.py
@@ -13,7 +11,7 @@ if __name__ == '__main__':
     ############################################################
 
     # Create a happy smiley, which is a subclass of Smiley
-    smiley = sad()
+    smiley = Sad()
 
     # This is a form of #polymorphism, as the Happy smiley class
     # does not have a method called .show(). This means that
