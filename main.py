60bc207a58bef93c7a3d8fd591f85c1fe0f6555a
Capeeslash <kenyoti.p@gmail.com>
2023-05-23 22:11:10 +0800

    Addressed requirements of the Portfolio

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-
 import time
 
 from happy import Happy
-from smiley import Smiley
 
 if __name__ == '__main__':
     # This is only needed if you have not deleted sense_hat.py
@@ -12,7 +10,7 @@ if __name__ == '__main__':
     ############################################################
 
     # Create a happy smiley, which is a subclass of Smiley
-    smiley = Happy
+    smiley = Happy()
 
     # This is a form of #polymorphism, as the Happy smiley class
     # does not have a method called .show(). This means that
