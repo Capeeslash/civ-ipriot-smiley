14d131465b9cbee4a51caa0fc00bf0352a7c6ce8
Capeeslash <kenyoti.p@gmail.com>
2023-05-23 22:39:56 +0800

    Addressed requirements of the Portfolio

--- angry.py
+++ angry.py
@@ -17,14 +17,19 @@ class Angry(Smiley):
         for pixel in mouth:
             self.pixels[pixel] = self.BLANK
 
+    def draw_eyebrows(self):
+        eyebrows = [10, 13, 18, 19, 20, 21]
+        for pixel in eyebrows:
+            self.pixels[pixel] = self.BLANK
+
     def draw_eyes(self, wide_open=True):
         """
         Method that draws the eyes (open or closed) on the standard smiley.
         :param wide_open: True if eyes opened, False otherwise
         """
-        eyes = [10, 13, 18, 21]
+        eyes = [26, 29]
         for pixel in eyes:
-            self.pixels[pixel] = self.BLANK if wide_open else self.complexion()
+            self.pixels[pixel] = self.GREEN if wide_open else self.complexion()
 
     def blink(self, delay=0.25):
         self.draw_eyes(wide_open=False)
